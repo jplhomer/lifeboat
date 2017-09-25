39d2a98f300775378f817abe9837ed3dd57e5eb4
Josh Larson <jplhomer@gmail.com>
2017-09-25 05:48:06 -0500

    Rename for it

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -7,8 +7,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'landing-page',
-      component: require('@/components/LandingPage')
+      name: 'dashboard',
+      component: require('@/components/Dashboard')
     },
     {
       path: '*',
