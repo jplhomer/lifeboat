ce045ef8ec296c869a76821270885ed73d0f75ce
Josh Larson <jplhomer@gmail.com>
2017-10-04 00:22:23 -0500

    Add sass-loader

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -6,7 +6,7 @@ import router from "./router";
 import store from "./store";
 import Docker from "./utils/docker";
 
-import "bulma/css/bulma.css";
+import "./assets/vars.scss";
 
 if (!process.env.IS_WEB) Vue.use(require("vue-electron"));
 Vue.http = Vue.prototype.$http = axios;
