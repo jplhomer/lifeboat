c30e7604fe7298917ef9dd7a0d8908b85b3e68fe
Josh Larson <jplhomer@gmail.com>
2017-10-03 22:23:14 -0500

    Quicker command queue

--- src/renderer/components/Dashboard/CommandTab.vue
+++ src/renderer/components/Dashboard/CommandTab.vue
@@ -10,7 +10,7 @@
           </div>
         </div>
         <div class="control is-expanded">
-          <input v-model="command" class="command-text input" type="text" @keyup.enter="run" @keyup.up="loadPreviousCommand" @keyup.down="loadNextCommand" :placeholder="`Type a command to run in ${service}...`">
+          <input v-model="command" class="command-text input" type="text" @keyup.enter="run" @keydown.up="loadPreviousCommand" @keydown.down="loadNextCommand" :placeholder="`Type a command to run in ${service}...`">
         </div>
         <div class="control">
           <button @click.prevent="run" class="button">Run</button>
