ec235bd2e1f760dc3ec43927c2eba29aeebd28f8
Josh Larson <jplhomer@gmail.com>
2017-11-10 18:01:22 -0600

    Quit when last window is closed on Mac

--- src/main/index.js
+++ src/main/index.js
@@ -48,9 +48,7 @@ function createWindow() {
 app.on("ready", createWindow);
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+  app.quit();
 });
 
 app.on("activate", () => {
