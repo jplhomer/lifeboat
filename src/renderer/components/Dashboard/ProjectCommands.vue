a7341f0293e3994ab2e420bb5d7a63a6fd65bbbf
Josh Larson <jplhomer@gmail.com>
2017-11-13 11:34:11 -0600

    Add base of run command to Vuex

--- src/renderer/components/Dashboard/ProjectCommands.vue
+++ src/renderer/components/Dashboard/ProjectCommands.vue
@@ -10,13 +10,13 @@
           </div>
         </div>
         <div class="control is-expanded">
-          <input v-model="command" class="command-text input" type="text" @keyup.enter="run" @keydown.up="loadPreviousCommand" @keydown.down="loadNextCommand" :placeholder="`Type a command to run in ${service}...`">
+          <input v-model="command" class="command-text input" type="text" @keyup.enter="run(project.id)" @keydown.up="loadPreviousCommand" @keydown.down="loadNextCommand" :placeholder="`Type a command to run in ${service}...`">
         </div>
         <div class="control">
-          <button @click.prevent="run" class="button">Run</button>
+          <button @click.prevent="run(project.id)" class="button">Run</button>
         </div>
         <div class="control">
-          <button @click.prevent="cancel" class="button is-danger" v-show="running">Cancel</button>
+          <button @click.prevent="cancel" class="button is-danger" v-show="running(project.id)">Cancel</button>
         </div>
       </div>
     </div>
@@ -25,7 +25,7 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 import AU from "ansi_up";
 import scrollToBottom from "@/mixins/scroll-to-bottom";
 const ansi_up = new AU();
@@ -45,7 +45,7 @@ export default {
       this.log += data;
       this.scrollToBottom();
     },
-    run() {
+    _run() {
       this.commandHistory.push(this.command);
       this.commandPointer = this.commandHistory.length;
 
@@ -99,15 +99,15 @@ export default {
       if (this.commandPointer < this.commandHistory.length) {
         this.command = this.commandHistory[++this.commandPointer];
       }
-    }
+    },
+
+    ...mapActions("ProjectCommand", ["run"])
   },
   computed: {
     services() {
       return this.project.services;
     },
-    running() {
-      return !!this.cmd;
-    },
+
     logOutput() {
       return ansi_up.ansi_to_html(this.logs(this.project.id));
     },
@@ -138,7 +138,7 @@ export default {
       }
     },
 
-    ...mapGetters("ProjectCommand", ["logs"])
+    ...mapGetters("ProjectCommand", ["logs", "running"])
   },
   created() {
     this.service = this.services[0];
--- src/renderer/store/modules/ProjectCommand.js
+++ src/renderer/store/modules/ProjectCommand.js
@@ -1,9 +1,18 @@
+import Vue from "vue";
+import Vuex from "vuex";
 import * as types from "../mutation-types";
+import Docker from "@/utils/docker";
+
+// Place to store command child processes run by projects
+const commands = {};
 
 const state = {
   commands: {},
-  services: {},
-  logs: {}
+  commandHistory: {},
+  commandPointer: {},
+  logs: {},
+  running: {},
+  services: {}
 };
 
 const mutations = {
@@ -13,6 +22,24 @@ const mutations = {
 
   [types.UPDATE_PROJECT_COMMAND_SERVICE](state, { id, service }) {
     state.services[id] = service;
+  },
+
+  [types.ADD_TO_PROJECT_COMMAND_HISTORY](state, id) {
+    const history = state.commandHistory[id] || [];
+    history.push(state.commands[id]);
+    state.commandHistory[id] = history;
+  },
+
+  [types.UPDATE_PROJECT_COMMAND_POINTER](state, { id, idx }) {
+    state.commandPointer[id] = idx;
+  },
+
+  [types.UPDATE_PROJECT_COMMAND_LOGS](state, { id, logs }) {
+    Vue.set(state.logs, id, logs);
+  },
+
+  [types.UPDATE_PROJECT_COMMAND_RUNNING](state, { id, running }) {
+    Vue.set(state.running, id, running);
   }
 };
 
@@ -25,18 +52,74 @@ const getters = {
     return state.logs[id] || "";
   },
 
+  running: state => id => {
+    return !!state.running[id];
+  },
+
   service: state => id => {
     return state.services[id];
   }
 };
 
 const actions = {
+  addLogs({ state, commit }, { id, logs }) {
+    commit(types.UPDATE_PROJECT_COMMAND_LOGS, {
+      id,
+      logs: state.logs[id] + logs
+    });
+  },
+
   setCommand({ commit }, { id, command }) {
     commit(types.UPDATE_PROJECT_COMMAND_COMMAND, { id, command });
   },
 
   setService({ commit }, { id, service }) {
     commit(types.UPDATE_PROJECT_COMMAND_SERVICE, { id, service });
+  },
+
+  run({ commit, getters, state, dispatch, rootGetters }, id) {
+    const project = rootGetters.projects[id];
+    const service = getters.service(id);
+    const command = getters.command(id);
+
+    // Record the current command
+    commit(types.ADD_TO_PROJECT_COMMAND_HISTORY, id);
+
+    // Update the command pointer to be able to cycle through
+    commit(types.UPDATE_PROJECT_COMMAND_POINTER, {
+      id,
+      idx: state.commandHistory[id].length
+    });
+
+    // Update the logs
+    commit(types.UPDATE_PROJECT_COMMAND_LOGS, {
+      id,
+      logs: `$ docker-compose run --rm ${service} ${command}\r\n`
+    });
+
+    // Mark the command as running
+    commit(types.UPDATE_PROJECT_COMMAND_RUNNING, { id, running: true });
+
+    // Run the command
+    commands[id] = Docker.run(project.dir, service, command.split(" "));
+
+    // Clear the command
+    commit(types.UPDATE_PROJECT_COMMAND_COMMAND, { id, command: "" });
+
+    // Listen for stdout data
+    commands[id].stdout.on("data", data => {
+      dispatch("addLogs", { id, logs: data.toString() });
+    });
+
+    // Listen for stderr data
+    commands[id].stderr.on("data", data => {
+      dispatch("addLogs", { id, logs: data.toString() });
+    });
+
+    // Clear the command when it's done
+    commands[id].on("exit", data => {
+      commit(types.UPDATE_PROJECT_COMMAND_RUNNING, { id, running: false });
+    });
   }
 };
 
