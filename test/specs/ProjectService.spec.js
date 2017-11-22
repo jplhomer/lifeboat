a7029e21206b0e16f7bdd87b99e6399e30d02244
Josh Larson <jplhomer@gmail.com>
2017-11-22 14:10:57 -0600

    Update test for ProjectService ports

--- test/specs/ProjectService.spec.js
+++ test/specs/ProjectService.spec.js
@@ -36,4 +36,18 @@ describe("ProjectService.vue", () => {
     expect(wrapper.findAll(".tags > span")).toHaveLength(1);
     expect(wrapper.find(".tags > span").text()).toBe("3000");
   });
+
+  it("should open a web browser when clicking on a port", () => {
+    wrapper.setComputed({
+      ports: [3000]
+    });
+
+    let openLocalhost = sinon.spy();
+    wrapper.setMethods({ openLocalhost });
+
+    wrapper.find(".tags > span").trigger("click");
+
+    expect(openLocalhost.called).toBe(true);
+    expect(openLocalhost.calledWith(3000)).toBe(true);
+  });
 });
